f45d9914abe4704927d46aea4c367ef22f58449f
Paul Garton <paulgarton1984@hotmail.com>
2025-02-13 20:30:19 +0000

    change set up so that navbar displays on all pages and add home link to nav

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,13 +6,12 @@ import ProjectsContainer from "./ui/home/projectsContainer";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Navbar />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Paul's Portfolio</h1>
         <Hero />
       </main>
-      <ProjectsContainer />
-
+      <section className="projects">
+        <ProjectsContainer />
+      </section>
       <Footer />
     </div>
   );
--- client/app/ui/navbar.tsx
+++ client/app/ui/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav>
+      <Link href="/">Home</Link>
       <Link href="/pages/blog">Blog</Link>
       <Link href="/pages/contact">Contact</Link>
     </nav>
